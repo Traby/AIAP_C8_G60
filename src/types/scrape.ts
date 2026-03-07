57489ce7faccfe30c2a3c204bff582c1d966c94a
Traby <truby@outlook.com>
2026-03-07 11:22:39 +0100

    Updated en.json

--- src/types/scrape.ts
+++ src/types/scrape.ts
@@ -3,7 +3,13 @@ export interface ScrapeResult {
   url: string;
   title: string;
   date: string;
+  category: string;
   summary: string;
   headings: string[];
   links: string[];
+}
+
+export interface Category {
+  id: string;
+  name: string;
 }
